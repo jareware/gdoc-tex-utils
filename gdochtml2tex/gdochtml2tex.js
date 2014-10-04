bac1780d483158fac2fa5cf608e8713d19585832
Jarno Rantanen <jarno@jrw.fi>
2014-10-04 16:29:38 +0300

    Initial (broken) version of a slightly less crappy gdochtml2tex.

--- gdochtml2tex/gdochtml2tex.js
+++ gdochtml2tex/gdochtml2tex.js
@@ -3,93 +3,92 @@ var fs = require('fs');
 var html = fs.readFileSync('/dev/stdin', 'utf-8').toString();
 var $ = cheerio.load(html);
 
-var $cursor = $('body > h1').prev();
-var output = '';
-var paragraphBuffer = '';
+var GOOGLE_URL_REDIR = /^https?:\/\/www\.google\.com\/url\?q=([^&]*).*$/;
 
-while (($cursor = $cursor.next()).length) {
-    output += getTex($cursor);
+//console.log(analyzeStyles($('head > style')));
+
+var tokens = tokenize($('body'), [], analyzeStyles($('head > style')));
+
+console.log(tokens);
+//console.log(tokens.join('\n'));
+
+function analyzeStyles($style) {
+
+    function extractClassName(forStyles) {
+        return ($style.text().match(new RegExp('\\.(\\w+)[\\s\n]*\\{[\\s\\n]*' + Object.keys(forStyles).map(function(key) {
+            return key + '\\s*:\\s*' + forStyles[key] + '\\s*;?';
+        }).join('') + '[\\s\\n]*\\}')) || [])[1];
+    }
+
+    return {
+        bold: extractClassName({ 'font-weight': 'bold' }),
+        italic: extractClassName({ 'font-style': 'italic' }),
+        underline: extractClassName({ 'text-decoration': 'underline' }),
+        monospace: extractClassName({ 'font-family': '"?Courier New"?' })
+    };
+
 }
 
-console.log(output);
+function tokenize($node, tokens, classMap) {
 
-function getTex($el) {
+    var end;
 
-    var el = $el[0];
-    var text = unescapeHTML($el.text().trim());
-    var LaTeX = '';
+    if ($node.length !== 1) {
+        throw new Error('tokenize() expects a singular cursor $node');
+    }
 
-    if (el.name === 'p' && $el.hasClass('title')) {
-        // The document title will currently have to be fixed into the template files
-    } else if (el.name === 'h1') {
-        LaTeX = '\\chapter{' + text + '}' /*+ '\n\\label{chapter:XXX}'*/;
-    } else if (el.name === 'h2') {
-        LaTeX = '\\section{' + text + '}';
-    } else if (el.name === 'h3') {
-        LaTeX = '\\subsection{' + text + '}';
-    } else if (el.name === 'h4') {
-        LaTeX = '\\subsubsection{' + text + '}';
-    } else if (el.name === 'p') {
-        if (text === '') { // an empty paragraph purges the buffer
-            LaTeX = ' '; // -> trigger the following paragraphBuffer check
-        } else {
-            paragraphBuffer += formatParagraph($el) + ' ';
-            return '';
-        }
+    if ($node.hasClass('title')) {
+        tokens.push('\\part{');
+        end = '}';
+    } else if ($node.get(0).name === 'h1') {
+        tokens.push('\\chapter{');
+        end = '}';
+    } else if ($node.get(0).name === 'h2') {
+        tokens.push('\\section{');
+        end = '}';
+    } else if ($node.get(0).name === 'h3') {
+        tokens.push('\\subsection{');
+        end = '}';
+    } else if ($node.get(0).name === 'h4') {
+        tokens.push('\\subsubsection{');
+        end = '}';
+    } else if ($node.get(0).name === 'a' && $node.attr('href')) {
+        tokens.push('\\href{' + unGoogleHref($node.attr('href')) + '}{');
+        end = '}';
+    } else if ($node.hasClass(classMap.bold)) {
+        tokens.push('\\textbf{');
+        end = '}';
+    } else if ($node.hasClass(classMap.italic)) {
+        tokens.push('\\emph{'); // or \textit
+        end = '}';
+    } else if ($node.hasClass(classMap.underline)) {
+        tokens.push('\\underline{');
+        end = '}';
+    } else if ($node.hasClass(classMap.bold)) {
+        tokens.push('\\textbf{');
+        end = '}';
+    }
+
+    if ($node.children().length === 0) { // leaf nodes contribute text
+        tokens.push($node.text().replace(/[\s]+/g, ' ').trim());
+    } else { // nodes with children are recursed
+        $node.children().each(function() {
+            tokenize($(this), tokens, classMap);
+        });
+    }
+
+    if (end) {
+        tokens.push(end);
+    }
+
+    return tokens;
+
+}
+
+function unGoogleHref(input) {
+    if (input.match(GOOGLE_URL_REDIR)) {
+        return decodeURIComponent(input.replace(GOOGLE_URL_REDIR, '$1'));
     } else {
-//        LaTeX = '\\bf{UNKNOWN INPUT:} ' + text;
-//        throw new Error('Unknown input: ' + $el.html());
+        return input;
     }
-
-    if (LaTeX && paragraphBuffer) { // if there's buffer & something other than paragraphs is to be emitted, purge the buffer first
-        LaTeX = paragraphBuffer + '\n\n' + LaTeX;
-        paragraphBuffer = '';
-    }
-
-    return LaTeX.trim() + '\n\n';
-
-}
-
-function formatParagraph($el) {
-
-    var LaTeX = '';
-
-    $el.find('span').each(function() {
-
-        var $this = $(this);
-        var text = unescapeHTML($this.text());
-
-        if ($this.hasClass('c4')) {
-            LaTeX += '{\\bf ' + text + '}';
-        } else if ($this.hasClass('c3')) {
-            LaTeX += '{\\em ' + text + '}';
-        } else {
-            LaTeX += text + '';
-        }
-
-    });
-
-    return LaTeX;
-
-}
-
-function unescapeHTML(text) {
-
-    [
-        [ /&amp;/g,     '\\&' ],
-        [ /&nbsp;/g,    ' ' ],
-        [ /&gt;/g,      '\\textgreater' ],
-        [ /&lt;/g,      '\\textless' ],
-        [ /&quot;/g,    '"' ],
-        [ /&ldquo;/g,   '"' ],
-        [ /&rdquo;/g,   '"' ],
-        [ /&rsquo;/g,   "'" ],
-        [ /&#39;/g,     "'" ],
-        [ /#/g,         '\\#']
-    ].forEach(function(translation) {
-        text = text.replace(translation[0], translation[1]);
-    });
-
-    return text;
-
 }
